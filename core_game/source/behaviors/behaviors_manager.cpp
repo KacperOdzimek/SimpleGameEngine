39909bf65949e1f80fdfabca60726184037d732d
KacperOdzimek <kacper-odzimek@wp.pl>
2024-02-04 02:21:35 +0100

    Removed Debug Function "stackDump"

--- core_game/source/behaviors/behaviors_manager.cpp
+++ core_game/source/behaviors/behaviors_manager.cpp
@@ -173,31 +173,6 @@ void behaviors::behaviors_manager::pass_float_arg(float arg)
     lua_pushnumber(impl->L, arg);
 }
 
-static void stackDump(lua_State* L) {
-    int top = lua_gettop(L);
-    for (int i = 1; i <= top; i++) {
-        printf("%d\t%s\t", i, luaL_typename(L, i));
-        switch (lua_type(L, i)) {
-        case LUA_TNUMBER:
-            printf("%g\n", lua_tonumber(L, i));
-            break;
-        case LUA_TSTRING:
-            printf("%s\n", lua_tostring(L, i));
-            break;
-        case LUA_TBOOLEAN:
-            printf("%s\n", (lua_toboolean(L, i) ? "true" : "false"));
-            break;
-        case LUA_TNIL:
-            printf("%s\n", "nil");
-            break;
-        default:
-            printf("%p\n", lua_topointer(L, i));
-            break;
-        }
-    }
-    printf("%s\n", "");
-}
-
 bool behaviors::behaviors_manager::prepare_call(behaviors::functions func, assets::behavior* bhv)
 {
     lua_getfield(impl->L, LUA_REGISTRYINDEX, bhv->name.c_str());
