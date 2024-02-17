02341e2758847dc432af3495213fa7ae51b285fe
KacperOdzimek <kacper-odzimek@wp.pl>
2024-02-17 18:15:45 +0100

    Database Functions Done

--- core_game/source/behaviors_shared/databases_functions.h
+++ core_game/source/behaviors_shared/databases_functions.h
@@ -6,12 +6,6 @@ namespace behaviors
 	{
 		namespace databases
 		{
-			int _d_set_f(lua_State* L);
-			int _d_set_i(lua_State* L);
-			int _d_set_b(lua_State* L);
-			int _d_set_s(lua_State* L);
-			int _d_set_e(lua_State* L);
-			int _d_get(lua_State* L);
 			void register_functions(lua_State* L);
 		}
 	}
