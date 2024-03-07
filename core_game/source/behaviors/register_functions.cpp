21d012a1e227cfd977ab3fad43bc9db300a0f7c0
KacperOdzimek <kacper-odzimek@wp.pl>
2024-03-07 21:00:45 +0100

    Removed databases functions

--- core_game/source/behaviors/register_functions.cpp
+++ core_game/source/behaviors/register_functions.cpp
@@ -3,7 +3,6 @@
 #include "source/behaviors_shared/utilities.h"
 
 #include "source/behaviors_shared/engine_functions.h"
-#include "source/behaviors_shared/databases_functions.h"
 #include "source/behaviors_shared/entities_functions.h"
 #include "source/behaviors_shared/add_component_functions.h"
 #include "source/behaviors_shared/components_functions.h"
@@ -13,7 +12,6 @@
 void behaviors::register_functions(lua_State* L)
 {
 	lua_functions::engine::register_functions(L);
-	//lua_functions::databases::register_functions(L);
 	lua_functions::entities::register_functions(L);
 	lua_functions::add_component::register_functions(L);
 	lua_functions::components::register_functions(L);
